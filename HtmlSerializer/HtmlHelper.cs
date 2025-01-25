54ee2916956a1831a485c07855175ff845912cda
“AyalaSpira” <a8552245@gmail.com>
2025-01-26 01:30:51 +0200

    עדכון הפרויקט - תיאור השינוי

--- HtmlSerializer/HtmlHelper.cs
+++ HtmlSerializer/HtmlHelper.cs
@@ -17,14 +17,6 @@ namespace HtmlSerializer
         public string[] SelfClosingTags { get; private set; }
 
 
-        //private HtmlHelper()
-        //{
-        //    string open = File.ReadAllText("D:\\Users\\User\\Desktop\\practicode2\\HtmlSerializer\\HtmlSerializer\\HtmlTags.json");
-        //    AllTags = JsonSerializer.Deserialize<string[]>(open);
-
-        //    string close = File.ReadAllText("D:\\Users\\User\\Desktop\\practicode2\\HtmlSerializer\\HtmlSerializer\\HtmlVoidTags.json");
-        //    SelfClosingTags = JsonSerializer.Deserialize<string[]>(close);
-        //}
         private HtmlHelper()
         {
             // טוענים את הנתונים מקבצי JSON
@@ -46,61 +38,6 @@ namespace HtmlSerializer
             }
         }
 
-        //public HtmlElement BuildHtmlTree(List<string> htmlStrings, List<string> allTags, List<string> selfClosingTags)
-        //{
-        //    var root = new HtmlElement { Name = "html" };
-        //    var currentElement = root;
-        //    HtmlElement parentElement = null;
-
-        //    foreach (var element in htmlStrings)
-        //    {
-        //        var parts = element.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-        //        var tagName = parts[0].Trim();
-
-        //        if (tagName == "html/")
-        //        {
-        //            break;
-        //        }
-        //        if (tagName.Contains("/"))
-        //        {
-        //            currentElement = currentElement.Parent;
-        //        }
-
-        //        else
-        //        {
-        //            HtmlElement a = new HtmlElement { Name = tagName };
-
-        //            a.Parent = currentElement;
-
-        //            foreach (var attribute in parts.Skip(1))
-        //            {
-        //                if (attribute.StartsWith("Id"))
-        //                {
-        //                    a.Id = attribute.Split('=')[1].Trim('"');
-        //                }
-        //                else if (attribute.StartsWith("Class"))
-        //                {
-        //                    a.Classes = attribute.Split('=')[1].Trim('"').Split(' ').ToList();
-        //                }
-
-        //            }
-
-        //            if (tagName.EndsWith("/"))
-        //            {
-        //                a.IsSelfClosing = true;
-        //                currentElement.Children.Add(a);
-        //            }
-        //            else
-        //            {
-        //                currentElement.Children.Add(a);
-        //                currentElement = a;
-        //            }
-        //        }
-        //    }
-        //    return root;
-
-        //}
 
 
 
